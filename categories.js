b3c4db816c628c384b717c2362b1915c306bf1ef
Kevin Poppe <kevin.poppe93@gmail.com>
2022-02-09 21:06:17 +0100

    Revert "trying to replace chars in target. No success for now"
    This reverts commit bbebe74bec047ef43cb577f580dbf9a402cadc06.

--- categories.js
+++ categories.js
@@ -1,18 +1,5 @@
 window.addEventListener('click', openModal);
 
 function openModal(e){
-    
-    let target = e.target.parentNode.attributes.id;
-    let targetString = "";
-
-    console.log(target)
-
-        targetString = target;
-
-    targetString = targetString.replace('id="', '') //ERROR
-
-    console.log(targetString)
-
-    console.log(document.querySelector('#tv'))
-
+    console.log(e.target.parentNode.attributes.id)
 }

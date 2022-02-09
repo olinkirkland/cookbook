bbebe74bec047ef43cb577f580dbf9a402cadc06
Kevin Poppe <kevin.poppe93@gmail.com>
2022-02-09 17:36:29 +0100

    trying to replace chars in target. No success for now

--- categories.js
+++ categories.js
@@ -1,5 +1,18 @@
 window.addEventListener('click', openModal);
 
 function openModal(e){
-    console.log(e.target.parentNode.attributes.id)
+    
+    let target = e.target.parentNode.attributes.id;
+    let targetString = "";
+
+    console.log(target)
+
+        targetString = target;
+
+    targetString = targetString.replace('id="', '') //ERROR
+
+    console.log(targetString)
+
+    console.log(document.querySelector('#tv'))
+
 }

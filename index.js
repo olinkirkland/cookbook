149ceba43b4b50d1fa0814c09bf493553792e73a
Olin Kirkland <me@olinkirk.land>
2022-02-10 15:58:43 +0100

    Add recipe data to index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const recipe1 = {
+  name: "Boiled Goose",
+  img: "assets/images/goose.jpg",
+  steps: [
+    {
+      title: "Step 1: Boil the goose!",
+      description: "Put the goose in a pot of hot water for 30 minutes",
+    },
+    {
+      title: "Step 2: Eat the goose!",
+      description: "Put the goose in your mouth and eat it",
+    },
+  ],
+};
+
+const recipe2 = {
+  name: "Boiled Goose",
+  img: "assets/images/goose.jpg",
+  steps: [
+    {
+      title: "Step 1: Boil the goose!",
+      description: "Put the goose in a pot of hot water for 30 minutes",
+    },
+    {
+      title: "Step 2: Eat the goose!",
+      description: "Put the goose in your mouth and eat it",
+    },
+  ],
+};
+
+const recipe3 = {
+  name: "Boiled Goose",
+  img: "assets/images/goose.jpg",
+  steps: [
+    {
+      title: "Step 1: Boil the goose!",
+      description: "Put the goose in a pot of hot water for 30 minutes",
+    },
+    {
+      title: "Step 2: Eat the goose!",
+      description: "Put the goose in your mouth and eat it",
+    },
+  ],
+};
+
+const allRecipes = { recipe1: recipe1, recipe2: recipe2, recipe3: recipe3 };

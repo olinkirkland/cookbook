f0b047029177b571bb4998776b5f24ce33d97700
Olin Kirkland <me@olinkirk.land>
2022-02-11 16:50:41 +0100

    Add Hauke's content, and ingredients, and add Kevin's modal code

--- recipe.js
+++ recipe.js
@@ -1,45 +1,122 @@
 const recipe1 = {
-  name: "Boiled Goose",
-  img: "assets/images/goose.jpg",
+  name: "Scrambled Eggs",
+  img: "assets/images/scrambled-eggs.jpg",
   steps: [
     {
-      title: "Step 1: Boil the goose!",
-      description: "Put the goose in a pot of hot water for 30 minutes",
+      title: "Step 1: Crack some eggs!",
+      description: "Crack the eggs into a bowl",
     },
     {
-      title: "Step 2: Eat the goose!",
-      description: "Put the goose in your mouth and eat it",
+      title: "Step 2: Add milk!",
+      description: "Add milk into the bowl",
+    },
+    {
+      title: "Step 3: Scramble the mixure!",
+      description: "Use the fork to scramble the egg/milk mixure",
+    },
+    {
+      title: "Step 4: Turn the stove on!",
+      description: "Turn the stovetop on",
+    },
+    {
+      title: "Step 5: Cook the egg/milk mixure!",
+      description: "Cook the egg/milk mixure in a pan on the stovetop",
+    },
+  ],
+  ingredients: [
+    {
+      name: "Pan",
+      amount: "1",
+    },
+    {
+      name: "Fork",
+      amount: "1",
+    },
+    {
+      name: "Stovetop",
+      amount: "1",
+    },
+    {
+      name: "Bowl",
+      amount: "1",
+    },
+    {
+      name: "Eggs",
+      amount: "2-3",
+    },
+    {
+      name: "milk",
+      amount: "1 Pinch",
     },
   ],
 };
 
 const recipe2 = {
-  name: "Olin's Favorite Feast",
-  img: "assets/images/goose.jpg",
+  name: "Raw Dragonfruit",
+  img: "assets/images/dragonfruit.jpg",
   steps: [
     {
-      title: "Step 1: Cook the feast",
-      description:
-        "Give me all the food though, make it exactly the way I want!",
+      title: "Step 1: Open dragonfruit!",
+      description: "Open the dragonfruit with a knife",
     },
     {
-      title: "Step 2: Prepare the chicken",
-      description: "Bok bok I am chicken.",
+      title: "Step 2: Consume!",
+      description: "Consume the dragonfruit",
+    },
+  ],
+  ingredients: [
+    {
+      name: "Human",
+      amount: "1",
+    },
+    {
+      name: "Dragonfruit",
+      amount: "1",
+    },
+    {
+      name: "Knife",
+      amount: "A",
     },
   ],
 };
 
 const recipe3 = {
-  name: "Boiled Goose",
-  img: "assets/images/goose.jpg",
+  name: "Fresh Pasta",
+  img: "assets/images/pasta.jpg",
   steps: [
     {
-      title: "Step 1: Boil the goose!",
-      description: "Put the goose in a pot of hot water for 30 minutes",
+      title: "Step 1: Make dough!",
+      description: "Add flour, water & eggs",
     },
     {
-      title: "Step 2: Eat the goose!",
-      description: "Put the goose in your mouth and eat it",
+      title: "Step 2: Roll out the dough!",
+      description: "Roll the dough out with a rolling pin",
+    },
+    {
+      title: "Step 2: Put dough in noodle machine!",
+      description: "Put the dough in the noodle machine",
+    },
+  ],
+  ingredients: [
+    {
+      name: "Noodle Machine",
+      amount: "1",
+    },
+    {
+      name: "water",
+      amount: "1 cup",
+    },
+    {
+      name: "flour",
+      amount: "1 cup",
+    },
+    {
+      name: "eggs",
+      amount: "2",
+    },
+    {
+      name: "Rolling pin",
+      amount: "1",
     },
   ],
 };
@@ -99,10 +176,22 @@ function populateRecipePage(recipeName) {
 
   if (recipe.ingredients) {
     recipe.ingredients.forEach((ingredient) => {
-      let ingredientEl = document.createElement("li");
+      let ingredientEl = document.createElement("span");
       ingredientEl.classList.add("mx-2");
-      let ingredientQuantityEl = document.createElement("span");
+      ingredientEl.classList.add("badge");
+      ingredientEl.classList.add("badge-info");
+      let ingredientAmountEl = document.createElement("span");
+      ingredientAmountEl.classList.add("text-muted");
+      ingredientAmountEl.classList.add("me-1");
       let ingredientNameEl = document.createElement("span");
+
+      ingredientAmountEl.textContent = ingredient.amount + ' ' + ingredient.name;
+      // ingredientNameEl.textContent = ingredient.name;
+
+      ingredientEl.appendChild(ingredientAmountEl);
+      ingredientEl.appendChild(ingredientNameEl);
+
+      document.getElementById("recipeIngredients").appendChild(ingredientEl);
     });
   }
 
